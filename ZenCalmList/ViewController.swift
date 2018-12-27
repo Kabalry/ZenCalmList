9dce6e63ead67aaa7f74664a01a1d1de71fb540f
KohseiAwachiDev <kohseiawachidev@kabalry-mbp.local>
2018-12-27 15:09:41 +0900

    Initial Commit

---
+++ ZenCalmList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZenCalmList
+//
+//  Created by KohseiAwachiDev on 2018/12/27.
+//  Copyright © 2018 Kohsei Awachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
